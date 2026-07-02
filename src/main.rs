a59c700a99725567dc64c729371a2190874311c5
Suhodolets Ilya <iliasuhodolets@gmail.com>
2026-07-02 09:02:26 +0300

    input buffer (interrupted flow WIP)

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,24 @@ mod parser;
 
 use errors::AppError;
 use lexer::Lexer;
-use std::io;
+use std::io::{self, ErrorKind, Read};
 
 fn main() -> Result<(), AppError> {
-    loop {
-        let mut buf = String::new();
-        let descriptor = io::stdin();
-        descriptor.read_line(&mut buf)?;
+    let mut buf = Vec::new();
+    let mut descriptor = io::stdin().lock();
+    // descriptor
+    //     .read_to_end(&mut buf)
+    //     .or_else(|err| match err.kind() {
+    //         ErrorKind::Interrupted => Ok(0),
+    //         error => return Err(error),
+    //     })?;
 
-        let lexems = Lexer(buf.chars()).get_lexems()?;
-        println!("{lexems:?}");
-    }
+    descriptor.read_to_end(&mut buf)?;
+
+    let s = buf.into_iter().map(char::from).collect::<String>();
+
+    let lexems = Lexer(s.chars()).get_lexems()?;
+    println!("{lexems:?}");
+
+    Ok(())
 }
